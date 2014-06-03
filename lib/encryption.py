99b9997990d591edb3120f05994ca01f217a5e7b
itgsod-Timothy-Holmsten <timothy.holmsten@elev.it-gymnasiet.se>
2014-06-03 11:10:25 +0200

    Updated the documentation

--- lib/encryption.py
+++ lib/encryption.py
@@ -1,4 +1,29 @@
+
+
 def encrypt(cleartext, offset):
+    """This function encrypts a string by using an offset and the alphabet.
+
+    :args:
+        cleartext (str): The string the function will try to encrypt.
+
+        offset (int): Offset is the amount the index should added upon.
+
+    :returns:
+        Returns an encrypted version of cleartext.
+
+    :raises:
+        :ValueError: If the string is empty.
+        :ValueError: If the offset is zero.
+
+
+    *Example:*
+        Use:
+            encrypt("grillkorv", 4)
+
+        Returns:
+            string: *"KVMPPOSVZ"*
+    """
+
     if len(cleartext) == 0:
         raise ValueError('can not encrypt empty string')
     if offset == 0:
@@ -11,8 +36,14 @@ def encrypt(cleartext, offset):
 
     for char in cleartext:
         if char.upper() in alphabet:
+            ## Takes the index of the letter from the list called alphabet
+            ## and adds the offset to that index and compares that with the
+            ## modulo of list's length.
             index = (alphabet.index(char.upper()) + offset) % len(alphabet)
+            ## This is the encrypted character it will add to the encrypted_str.
             new_char = alphabet[index]
+            ## This is the encrypted string it will later return, here it adds
+            ## the encrypted character to the string.
             encrypted_str += new_char
         else:
             encrypted_str += char
@@ -20,3 +51,5 @@ def encrypt(cleartext, offset):
 
 def decrypt(cleartext, offset):
     return encrypt(cleartext, -offset).lower()
+
+print(encrypt("grillkorv", 4))
